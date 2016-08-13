ad9e1b73dd0a147aa3bfe8f2d5aa14322e86b73c
= <danielmonteiro@id.uff.br>
2016-08-13 13:18:39 -0300

    No need to check for ICS anymore
    Since the minimum is Jelly Bean anyway.

--- KnightsOfAlentejoAndroid-AS/app/src/main/java/br/odb/menu/GameActivity.java
+++ KnightsOfAlentejoAndroid-AS/app/src/main/java/br/odb/menu/GameActivity.java
@@ -119,14 +119,11 @@ public class GameActivity extends Activity implements Updatable, OnItemSelectedL
 
 		boolean haveControllerPlugged = getGameControllerIds().size() > 0;
 
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
-
-
-			if (haveControllerPlugged && getActionBar() != null) {
-				getActionBar().setDisplayHomeAsUpEnabled(false);
-				getActionBar().hide();
-			}
+		if (haveControllerPlugged && getActionBar() != null) {
+			getActionBar().setDisplayHomeAsUpEnabled(false);
+			getActionBar().hide();
 		}
+
 
 		spinner = (Spinner) findViewById(R.id.spinner1);
 
