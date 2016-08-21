2b01680c548058f782a54c0130746b4645580ed8
= <danielmonteiro@id.uff.br>
2016-08-20 21:45:45 -0300

    Refactor to improve clarity

--- KnightsOfAlentejoAndroid-AS/app/src/main/java/br/odb/menu/GameActivity.java
+++ KnightsOfAlentejoAndroid-AS/app/src/main/java/br/odb/menu/GameActivity.java
@@ -103,6 +103,10 @@ public class GameActivity extends Activity implements Updatable, OnItemSelectedL
 
 		view.onResume();
 
+		enterImmersiveMode();
+	}
+
+	private void enterImmersiveMode() {
 		getWindow().getDecorView().setSystemUiVisibility(
 				 View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
 						| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
