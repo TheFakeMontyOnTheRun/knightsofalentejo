754fe08ff1d045cbc5b05b0743dfec56ee4aaa14
= <danielmonteiro@id.uff.br>
2016-08-20 22:36:30 -0300

    Try as hard as possible to stay on Immersive Mode

--- KnightsOfAlentejoAndroid-AS/app/src/main/java/br/odb/menu/GameActivity.java
+++ KnightsOfAlentejoAndroid-AS/app/src/main/java/br/odb/menu/GameActivity.java
@@ -112,7 +112,7 @@ public class GameActivity extends Activity implements Updatable, OnItemSelectedL
 						| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
 						| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
 						| View.SYSTEM_UI_FLAG_FULLSCREEN
-						| View.SYSTEM_UI_FLAG_IMMERSIVE);
+						| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 	}
 
 	/**
@@ -126,7 +126,6 @@ public class GameActivity extends Activity implements Updatable, OnItemSelectedL
 
 		this.level = getIntent().getIntExtra(KnightsOfAlentejoSplashActivity.MAPKEY_LEVEL_TO_PLAY, 0);
 
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
@@ -136,6 +135,7 @@ public class GameActivity extends Activity implements Updatable, OnItemSelectedL
 		mHaveController = getGameControllerIds().size() > 0;
 
 		if (mHaveController ) {
+			requestWindowFeature(Window.FEATURE_NO_TITLE);
 			setContentView(R.layout.game3dcontroller_layout);
 		} else {
 			setContentView(R.layout.game3d_layout);
@@ -437,6 +437,7 @@ public class GameActivity extends Activity implements Updatable, OnItemSelectedL
 
 		view.centerOn(view.getSelectedPlayer());
 		update();
+		enterImmersiveMode();
 	}
 
 	@Override
