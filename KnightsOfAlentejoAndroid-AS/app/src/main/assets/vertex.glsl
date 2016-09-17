5bf0d9e6a1a4d7e59bc04fe0cc47150cf26ccb46
= <danielmonteiro@id.uff.br>
2016-09-17 13:53:36 -0300

    Add a very faint fog

--- KnightsOfAlentejoAndroid-AS/app/src/main/assets/vertex.glsl
+++ KnightsOfAlentejoAndroid-AS/app/src/main/assets/vertex.glsl
@@ -4,6 +4,7 @@ attribute vec2 aTexCoord;
 uniform mat4 uModel;
 uniform mat4 uProjection;
 uniform mat4 uView;
+varying float distance;
 varying vec2 vTextureCoords;
 varying vec4 vColour;
 
@@ -11,4 +12,5 @@ void main() {
     gl_Position =  uProjection * uView * uModel * aPosition;
     vColour = aColour;
     vTextureCoords = aTexCoord;
+    distance = normalize( gl_Position.xyz).z;
 }
