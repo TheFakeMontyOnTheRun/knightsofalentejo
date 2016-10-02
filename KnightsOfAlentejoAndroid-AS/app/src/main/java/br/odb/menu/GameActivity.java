c5b6b098a0b3514698cb7637dc1509a1a86d4f3a
= <danielmonteiro@id.uff.br>
2016-10-02 11:40:38 -0300

    Disable the immersive mode
    since older Android versons kind of ignore some of those flags,
    resulting in an annoying experience

--- KnightsOfAlentejoAndroid-AS/app/src/main/java/br/odb/menu/GameActivity.java
+++ KnightsOfAlentejoAndroid-AS/app/src/main/java/br/odb/menu/GameActivity.java
@@ -270,15 +270,15 @@ public class GameActivity extends Activity implements OnItemSelectedListener, On
 	//presentation and interaction
 
 	private void enterImmersiveMode() {
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-				WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-		getWindow().getDecorView().setSystemUiVisibility(
-				View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-						| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-						| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-						| View.SYSTEM_UI_FLAG_FULLSCREEN
-						| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+//		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//
+//		getWindow().getDecorView().setSystemUiVisibility(
+//				View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//						| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//						| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+//						| View.SYSTEM_UI_FLAG_FULLSCREEN
+//						| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 	}
 
 	private void configureUiForInputDevice() {
